7169d048c1dc6806905797233558a3462ccb4f26
Alex <alexpardo2006@gmail.com>
2024-10-16 12:47:29 +0200

    Creacion del archivo

---
+++ soyelmejor.ps1
@@ -0,0 +1,5 @@
+﻿New-Item -ItemType Directory -Path C:/ -Name DirectorioAlex
+Set-Location -Path C:/DirectorioAlex
+New-Item -ItemType file -Name hola.txt
+Add-Content -Path hola.txt alex
+Copy-Item hola.txt hola.bak
